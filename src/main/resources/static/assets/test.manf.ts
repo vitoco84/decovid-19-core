49ade3d5e0ab03aafcc3882dd51e5334dccef43a
vito.colangelo <v.colangelo@hotmail.com>
2022-08-27 15:41:17 +0200

    added static content

---
+++ src/main/resources/static/assets/test.manf.ts
@@ -0,0 +1,142 @@
+export const testManfJson = [
+  {
+    display: 'AAZ-LMB, COVID-VIRO',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-17'
+  },
+  {
+    display: 'AESKU.DIAGNOSTICS GmbH & Co. KG, AESKU.RAPID SARS-CoV-2',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-06-18'
+  },
+  {
+    display: 'ArcDia International Ltd, mariPOC SARS-CoV-2',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-19'
+  },
+  {
+    display: 'Becton Dickinson, BD Veritor™ System for Rapid Detection of SARS CoV 2',
+    lang: 'en',
+    active: false,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-11'
+  },
+  {
+    display: 'BIOSYNEX S.A., BIOSYNEX COVID-19 Ag BSS',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'DIALAB GmbH, DIAQUICK COVID-19 Ag Cassette',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'GenSure Biotech Inc, GenSure COVID-19 Antigen Rapid Kit',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-06-14'
+  },
+  {
+    display: 'Green Cross Medical Science Corp., GENEDIA W COVID-19 Ag',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-11'
+  },
+  {
+    display: 'NanoEntek, FREND COVID-19 Ag',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-06-04'
+  },
+  {
+    display: 'New Gene (Hangzhou) Bioengineering Co., Ltd, COVID-19 Antigen Detection Kit',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-06-16'
+  },
+  {
+    display: 'Oncosem Onkolojik Sistemler San. ve Tic. A.S., CAT',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'PCL Inc, PCL COVID19 Ag Rapid FIA',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'PerGrande BioTech Development Co., Ltd., SARS-CoV-2 Antigen Detection Kit (Colloidal Gold Immunochromatographic Assay)',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-31'
+  },
+  {
+    display: 'Precision Biosensor, Inc, Exdia COVID-19 Ag',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'Quidel Corporation, Sofia SARS Antigen FIA',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'ScheBo Biotech AG, ScheBo SARS CoV-2 Quick Antigen',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-06-16'
+  },
+  {
+    display: 'SD BIOSENSOR Inc, STANDARD F COVID-19 Ag FIA',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'Sugentech, Inc, SGTi-flex COVID-19 Ag',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'TODA PHARMA, TODA CORONADIAG Ag',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-10'
+  },
+  {
+    display: 'Tody Laboratories Int., Coronavirus (SARS-CoV 2) Antigen - Oral Fluid',
+    lang: 'en',
+    active: true,
+    system: 'https://covid-19-diagnostics.jrc.ec.europa.eu/devices',
+    version: '2021-05-19'
+  }
+];
